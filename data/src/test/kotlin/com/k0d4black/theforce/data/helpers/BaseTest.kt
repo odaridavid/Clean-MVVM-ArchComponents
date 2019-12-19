edd670ba12289eea59e3ce24c20542448df19594
Davidodari <davidkibzodari@gmail.com>
2019-12-19 23:47:02 +0300

    remove runner from base test

--- data/src/test/kotlin/com/k0d4black/theforce/data/helpers/BaseTest.kt
+++ data/src/test/kotlin/com/k0d4black/theforce/data/helpers/BaseTest.kt
@@ -9,7 +9,7 @@ import org.junit.Before
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
+
 internal open class BaseTest {
 
     lateinit var mockWebServer: MockWebServer
