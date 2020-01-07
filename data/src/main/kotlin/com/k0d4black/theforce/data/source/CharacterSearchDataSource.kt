2b44b43a048a55f295b2c4109c86c1c45eb8f926
Davidodari <davidkibzodari@gmail.com>
2020-01-07 22:50:40 +0300

    refactor search with flow

--- data/src/main/kotlin/com/k0d4black/theforce/data/source/CharacterSearchDataSource.kt
+++ data/src/main/kotlin/com/k0d4black/theforce/data/source/CharacterSearchDataSource.kt
@@ -1,10 +1,11 @@
 package com.k0d4black.theforce.data.source
 
 import com.k0d4black.theforce.data.api.StarWarsApiService
+import com.k0d4black.theforce.data.mappers.toDomain
 import com.k0d4black.theforce.data.models.SearchedCharacterDataModel
-import com.k0d4black.theforce.domain.utils.Error
-import com.k0d4black.theforce.domain.utils.ResultWrapper
-import com.k0d4black.theforce.domain.utils.Success
+import com.k0d4black.theforce.domain.SearchedCharacterDomainModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class CharacterSearchDataSource @Inject constructor(private val apiService: StarWarsApiService) {
@@ -13,24 +14,19 @@ class CharacterSearchDataSource @Inject constructor(private val apiService: Star
      * Takes in [params] to be used for the search
      * @return list of search results
      */
-    suspend fun query(params: String): ResultWrapper<List<SearchedCharacterDataModel>> {
-        return try {
-            val searchResponse = apiService.searchCharacters(params)
-            val searchDataModels = mutableListOf<SearchedCharacterDataModel>()
-            for (searchResult in searchResponse.results) {
-                val searchedCharacterDataModel =
-                    SearchedCharacterDataModel(
-                        searchResult.name,
-                        searchResult.birthYear,
-                        searchResult.url
-                    )
-                searchDataModels.add(searchedCharacterDataModel)
-            }
-            Success(searchDataModels)
-        } catch (e: Exception) {
-            Error(e)
+    suspend fun query(params: String): Flow<List<SearchedCharacterDomainModel>> {
+        val searchResponse = apiService.searchCharacters(params)
+        val searchDataModels = mutableListOf<SearchedCharacterDataModel>()
+        for (searchResult in searchResponse.results) {
+            val searchedCharacterDataModel =
+                SearchedCharacterDataModel(
+                    searchResult.name,
+                    searchResult.birthYear,
+                    searchResult.url
+                )
+            searchDataModels.add(searchedCharacterDataModel)
         }
+        return flow { emit(searchDataModels.map { it.toDomain() }) }
     }
-
 
 }
