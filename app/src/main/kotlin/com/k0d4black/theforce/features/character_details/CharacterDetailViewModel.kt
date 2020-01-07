29d4ef226d04abd03cd4486b7879c4e789a64804
Davidodari <davidkibzodari@gmail.com>
2020-01-07 22:41:31 +0300

    refactor ui layer structure with ui state observer

--- app/src/main/kotlin/com/k0d4black/theforce/features/character_details/CharacterDetailViewModel.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/features/character_details/CharacterDetailViewModel.kt
@@ -2,13 +2,12 @@ package com.k0d4black.theforce.features.character_details
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.k0d4black.theforce.commons.Error
+import com.k0d4black.theforce.commons.Loading
+import com.k0d4black.theforce.commons.Success
+import com.k0d4black.theforce.commons.UiStateViewModel
 import com.k0d4black.theforce.data.usecases.CharacterDetailsUseCase
-import com.k0d4black.theforce.domain.utils.Error
-import com.k0d4black.theforce.domain.utils.Loading
-import com.k0d4black.theforce.domain.utils.ResultWrapper
-import com.k0d4black.theforce.domain.utils.Success
 import com.k0d4black.theforce.mappers.toPresentation
 import com.k0d4black.theforce.models.CharacterDetailsPresentationModel
 import com.k0d4black.theforce.models.FilmPresentationModel
@@ -20,16 +19,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CharacterDetailViewModel @Inject constructor(private val characterDetailsUseCase: CharacterDetailsUseCase) :
-    ViewModel() {
-
-    private val handler = CoroutineExceptionHandler { _, exception ->
-        _uiState.value = Error(exception)
-        println(exception.printStackTrace())
-    }
-    val uiState: LiveData<ResultWrapper<Any>>
-        get() = _uiState
-
-    private var _uiState = MutableLiveData<ResultWrapper<Any>>()
+    UiStateViewModel() {
 
     val characterDetail: LiveData<CharacterDetailsPresentationModel>
         get() = _characterDetail
@@ -70,8 +60,7 @@ class CharacterDetailViewModel @Inject constructor(private val characterDetailsU
             characterDetailsUseCase.getCharacterSpecies(characterId).collect {
                 _characterSpecies.value = it.map { species -> species.toPresentation() }
             }
-            _uiState.value = Success("Loading Complete")
+            _uiState.value = Success
         }
-
     }
 }
