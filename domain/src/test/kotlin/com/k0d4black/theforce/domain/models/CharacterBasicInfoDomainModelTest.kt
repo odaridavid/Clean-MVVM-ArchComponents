d730c305e758bc4c7a34495f362bf516af910427
Davidodari <davidkibzodari@gmail.com>
2020-01-10 12:48:35 +0300

    remove mock runner - dead code

--- domain/src/test/kotlin/com/k0d4black/theforce/domain/models/CharacterBasicInfoDomainModelTest.kt
+++ domain/src/test/kotlin/com/k0d4black/theforce/domain/models/CharacterBasicInfoDomainModelTest.kt
@@ -3,14 +3,11 @@ package com.k0d4black.theforce.domain.models
 import com.google.common.truth.Truth
 import com.k0d4black.theforce.domain.CharacterBasicInfoDomainModel
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
+
 internal class CharacterBasicInfoDomainModelTest {
 
     lateinit var expectedModel: CharacterBasicInfoDomainModel
-
 
     @Test
     fun `model instantiated has given values`() {
--- domain/src/test/kotlin/com/k0d4black/theforce/domain/models/CharacterSearchDomainModelTest.kt
+++ domain/src/test/kotlin/com/k0d4black/theforce/domain/models/CharacterSearchDomainModelTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.junit.MockitoJUnitRunner
 
-@RunWith(MockitoJUnitRunner::class)
+
 internal class CharacterSearchDomainModelTest {
 
     private lateinit var expectedModel: CharacterSearchDomainModel
