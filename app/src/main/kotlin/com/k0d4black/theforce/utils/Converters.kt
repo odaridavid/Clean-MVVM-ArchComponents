364487dfd4b582bb08658509c79c1e61e87ccbfd
David Odari <davidkibzodari@gmail.com>
2020-02-26 21:55:07 +0300

    Add about section (#59)
    * feat #57 :add about section
    
    * feat #57 :travis build fix

--- app/src/main/kotlin/com/k0d4black/theforce/utils/Converters.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/utils/Converters.kt
@@ -4,5 +4,9 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 
 
+/**
+ * Takes in a centimeters value from the domain model and on mapping to the presentation layer
+ * model it takes in both centimeters and assigns a converted value to height in inches property.
+ **/
 fun convertToInches(centimeters: String): String =
     (BigDecimal(centimeters.toDouble() * 0.393701).setScale(3, RoundingMode.HALF_EVEN)).toString()
