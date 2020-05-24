85d5f1412252274234c08ffc4c57ee969a12e2b0
Davidodari <davidkibzodari@gmail.com>
2020-05-24 22:33:47 +0300

    resolve failing tests due to module definitions

--- di/src/main/kotlin/com/k0d4black/theforce/modules/UseCaseModule.kt
+++ di/src/main/kotlin/com/k0d4black/theforce/modules/UseCaseModule.kt
@@ -1,20 +1,35 @@
 package com.k0d4black.theforce.modules
 
-import com.k0d4black.theforce.domain.usecases.GetFilmsUseCase
-import com.k0d4black.theforce.domain.usecases.GetPlanetUseCase
-import com.k0d4black.theforce.domain.usecases.GetSpeciesUseCase
-import com.k0d4black.theforce.domain.usecases.SearchCharactersUseCase
+import com.k0d4black.theforce.domain.repository.ICharacterDetailsRepository
+import com.k0d4black.theforce.domain.repository.ICharacterSearchRepository
+import com.k0d4black.theforce.domain.usecases.*
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val useCasesModule = module {
 
-    single(named("search")) { SearchCharactersUseCase(get()) }
+    single(named("search")) { provideSearchUseCase(get()) }
 
-    single(named("species")) { GetSpeciesUseCase(get()) }
+    single(named("species")) { provideSpeciesUseCase(get()) }
 
-    single(named("planet")) { GetPlanetUseCase(get()) }
+    single(named("planet")) { providePlanetUseCase(get()) }
 
-    single(named("films")) { GetFilmsUseCase(get()) }
+    single(named("films")) { provideFilmsUseCase(get()) }
 
+}
+
+fun provideSearchUseCase(searchRepository: ICharacterSearchRepository): SearchUseCase {
+    return SearchCharactersUseCase(searchRepository)
+}
+
+fun provideSpeciesUseCase(detailsRepository: ICharacterDetailsRepository): SpeciesUseCase {
+    return GetSpeciesUseCase(detailsRepository)
+}
+
+fun providePlanetUseCase(detailsRepository: ICharacterDetailsRepository): PlanetUseCase {
+    return GetPlanetUseCase(detailsRepository)
+}
+
+fun provideFilmsUseCase(detailsRepository: ICharacterDetailsRepository): FilmsUseCase {
+    return GetFilmsUseCase(detailsRepository)
 }
