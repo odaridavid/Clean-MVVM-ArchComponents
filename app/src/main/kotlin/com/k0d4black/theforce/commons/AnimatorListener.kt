20c869e3a6a934c27d3e1f8bd37b8a75f8a18b29
Davidodari <davidkibzodari@gmail.com>
2020-05-24 04:37:31 +0300

    code cleanup

--- app/src/main/kotlin/com/k0d4black/theforce/commons/AnimatorListener.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/commons/AnimatorListener.kt
@@ -3,26 +3,23 @@ package com.k0d4black.theforce.commons
 import android.animation.Animator
 
 class AnimatorListener(
-    private val onStart: ((Animator) -> (Unit))? = null,
-    private val onRepeat: ((Animator) -> (Unit))? = null,
-    private val onEnd: ((Animator) -> (Unit))? = null,
-    private val onCancel: ((Animator) -> (Unit))? = null
-) :
-    Animator.AnimatorListener {
-    override fun onAnimationRepeat(animator: Animator) {
-        onRepeat?.invoke(animator)
-    }
+    private val onEnd: ((Animator) -> (Unit))? = null
+) : Animator.AnimatorListener {
 
     override fun onAnimationEnd(animator: Animator) {
         onEnd?.invoke(animator)
     }
 
+    override fun onAnimationRepeat(animator: Animator) {
+        //no-op
+    }
+
     override fun onAnimationCancel(animator: Animator) {
-        onCancel?.invoke(animator)
+        //no-op
     }
 
     override fun onAnimationStart(animator: Animator) {
-        onStart?.invoke(animator)
+        //no-op
     }
 
 }
--- app/src/main/kotlin/com/k0d4black/theforce/features/settings/SettingsActivity.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/features/settings/SettingsActivity.kt
@@ -6,12 +6,7 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.k0d4black.theforce.R
 
-class SettingsActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-    }
+class SettingsActivity : AppCompatActivity(R.layout.activity_settings) {
 
     fun openAboutActivity(view: View) {
         val intent = Intent(this, AboutActivity::class.java)
