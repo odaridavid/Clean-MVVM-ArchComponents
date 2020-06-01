6f04d1b8d8b2a8690c0a4109dfafca592165884d
Davidodari <davidkibzodari@gmail.com>
2020-06-01 05:46:53 +0300

    add action bar

--- app/src/main/kotlin/com/k0d4black/theforce/features/settings/AboutActivity.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/features/settings/AboutActivity.kt
@@ -3,13 +3,18 @@ package com.k0d4black.theforce.features.settings
 import android.os.Bundle
 import com.k0d4black.theforce.R
 import com.k0d4black.theforce.base.BaseActivity
+import com.k0d4black.theforce.databinding.ActivityAboutBinding
 import com.mikepenz.aboutlibraries.LibsBuilder
 
 class AboutActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        val binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.aboutToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         val fragment = LibsBuilder()
             .withAboutIconShown(true)
             .supportFragment()
--- app/src/main/kotlin/com/k0d4black/theforce/features/settings/SettingsActivity.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/features/settings/SettingsActivity.kt
@@ -2,18 +2,20 @@ package com.k0d4black.theforce.features.settings
 
 import android.os.Bundle
 import android.view.View
-import com.k0d4black.theforce.R
 import com.k0d4black.theforce.base.BaseActivity
 import com.k0d4black.theforce.commons.startActivity
+import com.k0d4black.theforce.databinding.ActivitySettingsBinding
 
 class SettingsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        val binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.settingsToolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
-    fun openAboutActivity(view: View) {
-        startActivity<AboutActivity>()
-    }
+    fun openAboutActivity(view: View) = startActivity<AboutActivity>()
+
 }
