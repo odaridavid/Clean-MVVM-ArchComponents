940103cc2fcf50d585c7db792e629fbb6fcc0663
Davidodari <davidkibzodari@gmail.com>
2020-06-03 19:48:53 +0300

    move Di to app module

--- app/src/main/kotlin/com/k0d4black/theforce/di/NetworkModule.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/di/NetworkModule.kt
@@ -1,12 +1,26 @@
-package com.k0d4black.theforce.modules
+/**
+ *
+ * Copyright 2020 David Odari
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ **/
+package com.k0d4black.theforce.di
 
-import com.k0d4black.theforce.data.api.StarWarsApiService
+import com.k0d4black.theforce.data.remote.api.StarWarsApiService
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
+
 
 val networkModule = module {
 
@@ -22,7 +36,7 @@ val networkModule = module {
     single { provideOkHttpClient() }
 }
 
-private fun provideOkHttpClient(): OkHttpClient {
+internal fun provideOkHttpClient(): OkHttpClient {
     val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
@@ -33,15 +47,14 @@ private fun provideOkHttpClient(): OkHttpClient {
         .build()
 }
 
-private fun provideRetrofit(okHttpClient: OkHttpClient, url: String): Retrofit {
+internal fun provideRetrofit(okHttpClient: OkHttpClient, url: String): Retrofit {
     return Retrofit.Builder()
         .baseUrl(url)
         .client(okHttpClient)
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
 }
-
-private fun provideService(retrofit: Retrofit): StarWarsApiService =
+internal fun provideService(retrofit: Retrofit): StarWarsApiService =
     retrofit.create(StarWarsApiService::class.java)
 
 private fun provideBaseUrl(): String = "https://swapi.dev/api/"
--- app/src/main/kotlin/com/k0d4black/theforce/di/ViewModelModule.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/di/ViewModelModule.kt
@@ -1,20 +1,31 @@
 package com.k0d4black.theforce.di
 
-import com.k0d4black.theforce.features.character_details.CharacterDetailViewModel
-import com.k0d4black.theforce.features.character_search.CharacterSearchViewModel
+import com.k0d4black.theforce.viewmodel.CharacterDetailViewModel
+import com.k0d4black.theforce.viewmodel.DashboardViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val viewModelsModule = module {
 
-    viewModel { CharacterSearchViewModel(searchCharactersUseCase = get(named("search"))) }
+    viewModel {
+        DashboardViewModel(
+            searchCharactersUseCase = get(named("search")),
+            deleteAllFavoritesBaseUseCase = get(named("delete_all_favorite")),
+            deleteFavoriteBaseUseCase = get(named("delete_favorite")),
+            getAllFavoritesBaseUseCase = get(named("get_all_favorites"))
+        )
+    }
 
     viewModel {
         CharacterDetailViewModel(
             getFilmsUseCase = get(named("films")),
             getPlanetUseCase = get(named("planet")),
-            getSpeciesUseCase = get(named("species"))
+            getSpeciesUseCase = get(named("species")),
+            deleteFavoriteBaseUseCase = get(named("delete_favorite")),
+            insertFavoriteBaseUseCase = get(named("insert_favorite")),
+            getFavoriteByNameBaseUseCase = get(named("get_favorite_by_name")),
+            getFavoriteByIdBaseUseCase = get(named("get_favorite_by_id"))
         )
     }
 }
