8cdd603ebcf7f5a065dbdb5b0fb3c46c45e7161a
Davidodari <davidkibzodari@gmail.com>
2020-06-04 07:00:48 +0300

    DI cleanup

--- app/src/main/kotlin/com/k0d4black/theforce/TheForceApplication.kt
+++ app/src/main/kotlin/com/k0d4black/theforce/TheForceApplication.kt
@@ -1,11 +1,7 @@
 package com.k0d4black.theforce
 
 import android.app.Application
-import com.k0d4black.theforce.di.viewModelsModule
-import com.k0d4black.theforce.modules.repositoriesModule
-import com.k0d4black.theforce.modules.dataSourceModule
-import com.k0d4black.theforce.modules.networkModule
-import com.k0d4black.theforce.modules.useCasesModule
+import com.k0d4black.theforce.di.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -21,10 +17,11 @@ internal class TheForceApplication : Application() {
             modules(
                 networkModule,
                 viewModelsModule,
-                repositoriesModule,
-                dataSourceModule,
+                localDataSourceModule,
+                remoteDataSourceModule,
                 useCasesModule
             )
         }
     }
+
 }
