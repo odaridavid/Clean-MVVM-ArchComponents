eefb4a5c9d4b0346c1f704448e643764ee3f3862
Davidodari <davidkibzodari@gmail.com>
2020-06-09 18:27:29 +0300

    resolve clear text for release

--- data-remote/src/main/kotlin/com/k0d4black/theforce/data/remote/repository/CharacterDetailsRepository.kt
+++ data-remote/src/main/kotlin/com/k0d4black/theforce/data/remote/repository/CharacterDetailsRepository.kt
@@ -1,6 +1,7 @@
 package com.k0d4black.theforce.data.remote.repository
 
 import com.k0d4black.theforce.data.remote.api.StarWarsApiService
+import com.k0d4black.theforce.data.remote.enforceHttps
 import com.k0d4black.theforce.data.remote.mappers.toDomain
 import com.k0d4black.theforce.domain.models.Film
 import com.k0d4black.theforce.domain.models.Planet
@@ -14,26 +15,26 @@ class CharacterDetailsRepository(
 ) : ICharacterDetailsRepository {
 
     override suspend fun getCharacterPlanet(characterUrl: String): Flow<Planet> = flow {
-        val planetResponse = apiService.getPlanet(characterUrl)
-        val planet = apiService.getPlanetDetails(planetResponse.homeworldUrl)
+        val planetResponse = apiService.getPlanet(characterUrl.enforceHttps())
+        val planet = apiService.getPlanetDetails(planetResponse.homeworldUrl.enforceHttps())
         emit(planet.toDomain())
     }
 
     override suspend fun getCharacterSpecies(characterUrl: String): Flow<List<Specie>> = flow {
-        val speciesResponse = apiService.getSpecies(characterUrl)
+        val speciesResponse = apiService.getSpecies(characterUrl.enforceHttps())
         val species = mutableListOf<Specie>()
         for (specieUrl in speciesResponse.specieUrls) {
-            val specie = apiService.getSpecieDetails(specieUrl)
+            val specie = apiService.getSpecieDetails(specieUrl.enforceHttps())
             species.add(specie.toDomain())
         }
         emit(species)
     }
 
     override suspend fun getCharacterFilms(characterUrl: String): Flow<List<Film>> = flow {
-        val filmsResponse = apiService.getFilms(characterUrl)
+        val filmsResponse = apiService.getFilms(characterUrl.enforceHttps())
         val films = mutableListOf<Film>()
         for (filmUrl in filmsResponse.filmUrls) {
-            val film = apiService.getFilmDetails(filmUrl)
+            val film = apiService.getFilmDetails(filmUrl.enforceHttps())
             films.add(film.toDomain())
         }
         emit(films)
