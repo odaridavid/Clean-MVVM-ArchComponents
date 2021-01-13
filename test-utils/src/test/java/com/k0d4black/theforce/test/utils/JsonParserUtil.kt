7483deb42ab672011f313a3eddb783fd5585630f
David Odari <davidkibzodari@gmail.com>
2021-01-14 00:52:46 +0100

    cleanup test-utils module

--- test-utils/src/test/java/com/k0d4black/theforce/test/utils/JsonParserUtil.kt
+++ test-utils/src/test/java/com/k0d4black/theforce/test/utils/JsonParserUtil.kt
@@ -1,8 +1,9 @@
-package com.k0d4black.theforce.test.utils.helpers
+package com.k0d4black.theforce.test.utils
 
 import com.google.common.io.Resources.getResource
 import java.io.File
 
+//TODO Use something else
 internal fun getJson(path: String): String {
     val uri = getResource(path)
     val file = File(uri.path)
--- test-utils/src/test/java/com/k0d4black/theforce/test/utils/StarWarsRequestDispatcher.kt
+++ test-utils/src/test/java/com/k0d4black/theforce/test/utils/StarWarsRequestDispatcher.kt
@@ -20,58 +20,35 @@ import com.k0d4black.theforce.test.utils.Constants.NON_EXISTANT_CHARACTER_URL
 import com.k0d4black.theforce.test.utils.Constants.NON_EXISTENT_SEARCH_PARAMS
 import com.k0d4black.theforce.test.utils.Constants.PLANET_URL
 import com.k0d4black.theforce.test.utils.Constants.SPECIES_URL
-import com.k0d4black.theforce.test.utils.helpers.getJson
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
 import java.net.HttpURLConnection
 
-/**
- * Handles Requests from mock web server
- */
 internal class StarWarsRequestDispatcher : Dispatcher() {
 
     override fun dispatch(request: RecordedRequest): MockResponse {
         return when (request.path) {
             "/people/?search=$EXISTING_SEARCH_PARAMS" -> {
-                MockResponse()
-                    .setResponseCode(HttpURLConnection.HTTP_OK)
-                    .setBody(getJson("json/character_search.json"))
+                setSuccessfulResponseBody(filePath = "json/character_search.json")
             }
             "/people/?search=$NON_EXISTENT_SEARCH_PARAMS" -> {
-                MockResponse()
-                    .setResponseCode(HttpURLConnection.HTTP_OK)
-                    .setBody(
-                        getJson("json/character_search_no_match.json")
-                    )
+                setSuccessfulResponseBody(filePath = "json/character_search_no_match.json")
             }
             NON_EXISTANT_CHARACTER_URL -> {
                 MockResponse()
                     .setResponseCode(HttpURLConnection.HTTP_NOT_FOUND)
                     .setBody(getJson("json/not_found.json"))
             }
-            EXISTING_CHARACTER_URL -> {
-                MockResponse()
-                    .setResponseCode(HttpURLConnection.HTTP_OK)
-                    .setBody(getJson("json/character_details.json"))
-            }
-            SPECIES_URL -> {
-                MockResponse()
-                    .setResponseCode(HttpURLConnection.HTTP_OK)
-                    .setBody(getJson("json/characters_species.json"))
-            }
-            FILM_URL -> {
-                MockResponse()
-                    .setResponseCode(HttpURLConnection.HTTP_OK)
-                    .setBody(getJson("json/character_films.json"))
-            }
-            PLANET_URL -> {
-                MockResponse()
-                    .setResponseCode(HttpURLConnection.HTTP_OK)
-                    .setBody(getJson("json/character_planet.json"))
-            }
+            EXISTING_CHARACTER_URL -> setSuccessfulResponseBody(filePath = "json/character_details.json")
+            SPECIES_URL -> setSuccessfulResponseBody(filePath = "json/characters_species.json")
+            FILM_URL -> setSuccessfulResponseBody(filePath = "json/character_films.json")
+            PLANET_URL -> setSuccessfulResponseBody(filePath = "json/character_planet.json")
             else -> throw IllegalArgumentException("Unknown Request Path ${request.path.toString()}")
         }
     }
 
+    private fun setSuccessfulResponseBody(filePath: String) = MockResponse()
+        .setResponseCode(HttpURLConnection.HTTP_OK)
+        .setBody(getJson(filePath))
 }
