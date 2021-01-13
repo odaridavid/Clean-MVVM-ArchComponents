28a698ab40bda5ba441629f01a7ce663687874fc
David Odari <davidkibzodari@gmail.com>
2021-01-14 00:34:40 +0100

    create Result class

--- shared/src/main/java/com/k0d4black/theforce/shared/model/Result.kt
+++ shared/src/main/java/com/k0d4black/theforce/shared/model/Result.kt
@@ -1,4 +1,14 @@
 package com.k0d4black.theforce.shared.model
 
-sealed class Result {
+sealed class Result<out Type> {
+
+    data class Success<out Type>(
+        val data: Type,
+        val statusMessage: String? = null,
+        val statusCode: Int = -1
+    ) : Result<Type>()
+
+    data class Error(val throwable: Throwable) : Result<Nothing>()
+
+    object Loading : Result<Nothing>()
 }
