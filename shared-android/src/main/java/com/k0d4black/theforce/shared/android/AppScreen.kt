a2dfe0578af90a079495ecab6bba0d1bd01c2c15
David Odari <davidkibzodari@gmail.com>
2021-01-22 23:40:58 +0100

    setup app screens destination constants and manifest with activities

--- shared-android/src/main/java/com/k0d4black/theforce/shared/android/AppScreen.kt
+++ shared-android/src/main/java/com/k0d4black/theforce/shared/android/AppScreen.kt
@@ -1,17 +1,27 @@
 package com.k0d4black.theforce.shared.android
 
 enum class AppScreen(val classPath: String) {
-    ABOUT(classPath = "com.k0d4black.theforce.feature.about.AboutActivity"),
+    ABOUT(
+        classPath = "com.k0d4black.theforce.feature.about.ui.AboutActivity"
+    ),
     SETTINGS(
-        classPath = "com.k0d4black.theforce.feature.settings.SettingsFragment"
+        classPath = "com.k0d4black.theforce.feature.settings.ui.SettingsFragment"
     ),
     CHARACTER_SEARCH(
         classPath = "com.k0d4black.theforce.feature.charactersearch.ui.CharacterSearchFragment"
     ),
+    CHARACTER_SEARCH_RESULTS(
+        classPath = "com.k0d4black.theforce.feature.charactersearchresults.ui.CharacterSearchResultsActivity"
+    ),
     CHARACTER_DETAILS(
         classPath = "com.k0d4black.theforce.feature.characterdetails.ui.CharacterDetailsActivity"
     ),
-    HOME(classPath = "com.k0d4black.theforce.feature.home.ui.HomeActivity"),
+    RECENT_CHARACTER_SEARCH(
+        classPath = "com.k0d4black.theforce.feature.recentcharactersearch.ui.RecentCharacterSearchFragment"
+    ),
+    HOME(
+        classPath = "com.k0d4black.theforce.feature.home.ui.HomeActivity"
+    ),
     FAVORITES(
         classPath = "com.k0d4black.theforce.feature.favoritecharacters.ui.FavoriteCharactersFragment"
     )
