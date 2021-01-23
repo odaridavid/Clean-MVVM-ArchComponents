25aeb2b4af191820f45e82c4ca66381429abfeb9
David Odari <davidkibzodari@gmail.com>
2021-01-23 05:06:40 +0100

    flesh out favorites ui

--- feature-favorite-characters/src/main/java/com/k0d4black/theforce/feature/favoritecharacters/ui/FavoriteCharactersFragment.kt
+++ feature-favorite-characters/src/main/java/com/k0d4black/theforce/feature/favoritecharacters/ui/FavoriteCharactersFragment.kt
@@ -1,5 +1,21 @@
 package com.k0d4black.theforce.feature.favoritecharacters.ui
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.k0d4black.theforce.feature.favoritecharacters.databinding.FragmentFavoritesBinding
+import com.k0d4black.theforce.shared.android.base.BindingFragment
 
-class FavoriteCharactersFragment : Fragment()
+class FavoriteCharactersFragment : BindingFragment<FragmentFavoritesBinding>() {
+
+    // region BindingFragment
+
+    override fun inflateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): FragmentFavoritesBinding =
+        FragmentFavoritesBinding.inflate(inflater, container, false)
+
+    // endregion
+}
